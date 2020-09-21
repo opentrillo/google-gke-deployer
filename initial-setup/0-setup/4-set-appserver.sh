22ecb81b789807db79d273ee42ccb2c56bd8b57e
root <root@gke-mp-builder.c.trillo-k8s-public.internal>
2020-09-21 23:26:02 +0000

    updaes

--- initial-setup/0-setup/4-set-appserver.sh
+++ initial-setup/0-setup/4-set-appserver.sh
@@ -10,6 +10,9 @@ fi
 
 source ../env-global.sh
 
+gcloud compute addresses create trillort-ip --global
+export APPSERVER_IP=$(gcloud compute addresses describe trillort-ip --global --format="value(address)")
+
 #Set the default admin password for the appserver
 export APPSERVER_DNS_NAME=${APPSERVER_NAME}
 export APPSERVER_DEFAULT_PASSWORD=$(openssl rand -base64 12)
