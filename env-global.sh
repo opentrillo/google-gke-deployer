c26c714c73771bc433ef9d2a175010227bde1229
Saqib Awan <saawan@trillo.io>
2022-03-22 23:50:10 -0700

    nodes of the cluster are provision-able. Size is increased as well.

--- env-global.sh
+++ env-global.sh
@@ -76,7 +76,9 @@ export DEFAULT_APP=main
 #SFTP
 export SFTP_MACHINE_TYPE=n1-standard-2
 export DB_MACHINE_TYPE=db-n1-standard-2
-export GKE_MACHINE_TYPE=n1-standard-2
+export GKE_MACHINE_TYPE=n1-standard-4
+export GKE_NODES=1 #This can be increased to 2
+
 
 #Trillo GKE Marketplace license
 #export TRILLO_LICENSE_MODE=evaluation
