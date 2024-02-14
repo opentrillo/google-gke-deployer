589f41d541dffd98e324414039f77310802b8275
Saqib Awan <saawan@trillo.io>
2024-02-13 16:01:25 -0800

    updates

--- deployment/4-gke/1-create-cluster.sh
+++ deployment/4-gke/1-create-cluster.sh
@@ -28,7 +28,7 @@ gcloud compute routers nats create $CLOUD_NAT_ROUTER_CONFIG \
 # Creating GKE Private Cluster
 # for production, set master-authorized-networks="${DEPLOYMENTIP}/32
 
-gcloud container clusters create ${CLUSTER} --region ${REGION} --machine-type "${GKE_MACHINE_TYPE}" --network "${NETWORK}" --subnetwork "${SUBNETWORK}" --num-nodes "${GKE_NODES_PER_ZONE}" --min-nodes "${GKE_NODES}" --max-nodes "${GKE_NODES}" --addons HorizontalPodAutoscaling,HttpLoadBalancing,GcePersistentDiskCsiDriver --enable-ip-alias --scopes storage-rw --enable-shielded-nodes --enable-private-nodes --master-ipv4-cidr "172.14.0.0/28" --enable-master-authorized-networks --master-authorized-networks="0.0.0.0/0" --node-locations "$ZONE","$ZONE2" --enable-autoupgrade --enable-autorepair
+gcloud container clusters create ${CLUSTER} --region ${REGION} --machine-type "${GKE_MACHINE_TYPE}" --network "${NETWORK}" --subnetwork "${SUBNETWORK}" --num-nodes "${GKE_NODES_PER_ZONE}" --addons HorizontalPodAutoscaling,HttpLoadBalancing,GcePersistentDiskCsiDriver --enable-ip-alias --scopes storage-rw --enable-shielded-nodes --enable-private-nodes --master-ipv4-cidr "172.14.0.0/28" --enable-master-authorized-networks --master-authorized-networks="0.0.0.0/0" --node-locations "$ZONE","$ZONE2" --enable-autoupgrade --enable-autorepair
 
 
 touch $COMPLETED
